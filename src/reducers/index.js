bc80c06dfe51529ea3d52dd49c7dd85b5b99c7e7
Marcio Lisboa <mjlbnu@gmail.com>
2022-10-07 22:50:45 -0300

    First commit

--- src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import authReducer from "./AuthReducer";
-import statementsReducer from "./StatementsReducer";
-
-export const reducers = combineReducers({ authReducer, statementsReducer });
