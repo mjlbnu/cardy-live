7c1df59ae36c60a019ff0908aaa49480b7fc2342
Marcio Lisboa <mjlbnu@gmail.com>
2022-11-14 23:18:34 -0300

    Fix progressBar

--- client/src/components/Timer/Timer.jsx
+++ client/src/components/Timer/Timer.jsx
@@ -2,26 +2,35 @@ import React from "react";
 import { useRef } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import './Timer.css';
+import "./Timer.css";
 
 function Timer() {
-  const [seconds, setSeconds] = useState(30)
+  const [seconds, setSeconds] = useState(30);
   const progressbar = useRef();
 
   useEffect(() => {
     if (seconds === 0) return;
     const oneSecond = 1000;
     const interval = setInterval(() => {
-      setSeconds((seconds) => seconds -1);
+      setSeconds((seconds) => seconds - 1);
     }, oneSecond);
     return () => clearInterval(interval);
   }, [seconds]);
 
   return (
-    <div className="container">
+    <div
+      className="timer"
+      style={{
+        display: seconds > 0 ? "block" : "none",
+      }}
+    >
       <div className="label">{seconds}</div>
       <div className="border">
-        <div ref={progressbar} id="#progressbar" className="progressbar animate"></div>
+        <div
+          ref={progressbar}
+          id="#progressbar"
+          className={`progressbar ${seconds > 0 ? "animate" : ""}`}
+        ></div>
       </div>
     </div>
   );
