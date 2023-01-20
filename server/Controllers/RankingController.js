a60f1359be8eb19a711b2624ae7b94870735ed75
Lisboa <Marcio.Lisboa@philips.com>
2023-01-20 16:37:07 -0300

    Ranking query

--- server/Controllers/RankingController.js
+++ server/Controllers/RankingController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import RankingModel from "../Models/RankingModel.js";
 import UserModel from "../Models/UserModel.js";
 
@@ -12,22 +13,54 @@ export const getRanking = async (req, res) => {
 
 export const getRankingAgr = async (req, res) => {
   try {
+    
+    
+    const ranking = await RankingModel.aggregate([
+      {
+        '$lookup': {
+          //searching collection name
+          'from': 'users',
+          //setting variable [searchId] where your string converted to ObjectId
+          'let': { "searchId": { $toObjectId: "$userId" } },
+
+          //search query with our [searchId] value
+          "pipeline": [
+            //searching [searchId] value equals your field [_id]
+            { "$match": { "$expr": [{ "_id": "$$searchId" }] } },
+            //projecting only fields you reaaly need, otherwise you will store all - huge data loads
+            { "$project": { "firstname": 1 } }
+
+          ],
+          localField: "userId",
+          foreignField: "_id",
+
+          'as': 'user'
+        }
+      },
+    ]);
+    
     /*
     const ranking = await RankingModel.aggregate([
       {
+        $match: {
+          _id: new mongoose.Types.ObjectId("63c1f731e9a0cf579bbfd3f7"),
+        },
+      },
+      
+      {
         $lookup: {
-          from: "users", localField: "userId",
-          foreignField: "_id", as: "user"
+          from: "users",
+          localField: "userId",
+          foreignField: "lastname", as: "user"
         }
       }
+      
     ]);
     */
-    
-    //const ranking = await RankingModel.aggregate([{$lookup:{from:"users",localField:"rankings.userId",foreignField:"users._id", as:"user"}}]);
-    const ranking = await RankingModel.aggregate([{$lookup:{from:"users",localField:"rankings.userId",foreignField:"users._id", as:"user"}}]);
+   
     res.status(200).json(ranking);
   } catch (error) {
-    res.status(404).json("No ranking found");
+    res.status(500).json({ message: error.message });
   }
 };
 
