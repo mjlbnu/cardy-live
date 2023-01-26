8a800a07c61aadf5c1dad0fdcd957347c0d6af7c
Lisboa <Marcio.Lisboa@philips.com>
2023-01-26 16:48:10 -0300

    Profile card

--- client/src/components/ProfileCard/ProfileCard.jsx
+++ client/src/components/ProfileCard/ProfileCard.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Cover from "../../img/cover2.jpg";
 import ProfileImg from "../../img/caco.jpg";
 import "./ProfileCard.css";
 import ProfileModal from "../ProfileModal/ProfileModal";
 import Statements from "../Statements/Statements";
-import { useSelector, useDispatch } from  "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { getProfile } from "../../actions/UserAction";
 
 function ProfileCard() {
   const dispatch = useDispatch();
@@ -12,40 +13,39 @@ function ProfileCard() {
   const [modalStOpened, setModalStOpened] = useState(false);
   const { profile, loading } = useSelector((state) => state.profileReducer);
 
-if (!profile) return null;
+  useEffect(() => {
+    dispatch(getProfile())
+  })
+
+  if (!profile) return null;
 
   return (
     <div className="ProfileCard">
-      <div className="ProfileImages">
-        <img src={Cover} alt="" />
-        <img src={ProfileImg} alt="" />
-      </div>
-      <div className="ProfileName">
-        <span>${profile.firstname} ${profile.lastname}</span>
-        <span>Noob</span>
-      </div>
-      <div className="RoleStatus">
-        <hr />
-        <div>
-          <div className="Role">
-            <span>{profile.isAdmin ? "Admin" : "Player"}</span>
-            <span>Role</span>
-          </div>
-          <div className="VerticalLine"></div>
-          <div className="Role">
-            <span>{profile.ranking[0].points || "0"}</span>
-            <span>Points</span>
-          </div>
-        </div>
-        <hr />
-      </div>
-      <span onClick={() => setModalOpened(true)}>My Profile</span>
-      <ProfileModal modalOpened={modalOpened} setModalOpened={setModalOpened} />
-      <span onClick={() => setModalStOpened(true)}>My Statements</span>
-      <Statements
-        modalStOpened={modalStOpened}
-        setModalStOpened={setModalStOpened}
-      />
+      {loading
+        ? "Fetching profile"
+        : <><div className="ProfileImages">
+          <img src={Cover} alt="" />
+          <img src={ProfileImg} alt="" />
+        </div><div className="ProfileName">
+            <span>${profile.firstname} ${profile.lastname}</span>
+            <span>Noob</span>
+          </div><div className="RoleStatus">
+            <hr />
+            <div>
+              <div className="Role">
+                <span>{profile.isAdmin ? "Admin" : "Player"}</span>
+                <span>Role</span>
+              </div>
+              <div className="VerticalLine"></div>
+              <div className="Role">
+                <span>{profile.ranking[0].points || "0"}</span>
+                <span>Points</span>
+              </div>
+            </div>
+            <hr />
+          </div><span onClick={() => setModalOpened(true)}>My Profile</span><ProfileModal modalOpened={modalOpened} setModalOpened={setModalOpened} /><span onClick={() => setModalStOpened(true)}>My Statements</span><Statements
+            modalStOpened={modalStOpened}
+            setModalStOpened={setModalStOpened} /></>}
     </div>
   );
 }
