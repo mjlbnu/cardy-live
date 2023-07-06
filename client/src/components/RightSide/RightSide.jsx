fbcd8c8d7b20528d3a5c6bea5a2387f700cb2d6c
mjlbnu <mjlbnu@gmail.com>
2023-07-05 23:49:13 -0300

    DropDownMenu

--- client/src/components/RightSide/RightSide.jsx
+++ client/src/components/RightSide/RightSide.jsx
@@ -1,22 +1,69 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./RightSide.css";
 import { UilSetting } from "@iconscout/react-unicons";
 import { UilHome } from "@iconscout/react-unicons";
 import { UilUser } from "@iconscout/react-unicons";
+import { UilSignout } from "@iconscout/react-unicons";
+import { UilEdit } from "@iconscout/react-unicons";
 import Chat from "../../img/comment.png";
 import Ranking from "../Ranking/Ranking";
 
 function RightSide() {
+  const [open, setOpen] = useState(false);
+
+  let menuRef = useRef();
+
+  useEffect(() => {
+    let handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setOpen(false);
+        console.log(menuRef.current);
+      }
+    };
+
+    document.addEventListener("mousedown", handler);
+
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  });
+
   return (
     <div className="RightSide">
       <div className="nav-icons">
-        <UilHome />
         <UilUser />
-        <img src={Chat} alt="" />
-        <UilSetting />
+        <UilSignout />
+        <UilSetting
+          className="menu-trigger"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        />
+        <div
+          className={`dropdown-menu ${open ? "active" : "inactive"}`}
+          onMouseLeave={() => {
+            setOpen(!open);
+          }}
+        >
+          <ul>
+            <DropdownItem img={<UilUser />} text={"My Profile"} />
+            <DropdownItem img={<UilEdit />} text={"Edit Profile"} />
+            <DropdownItem img={<UilSetting />} text={"Settings"} />
+            <DropdownItem img={<UilSignout />} text={"Logout"} />
+          </ul>
+        </div>
       </div>
       <Ranking />
     </div>
+  );
+}
+
+function DropdownItem(props) {
+  return (
+    <li className="dropdownItem">
+      {props.img}
+      <a> {props.text} </a>
+    </li>
   );
 }
 
