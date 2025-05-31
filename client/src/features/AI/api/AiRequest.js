e34fbd97436f3a4c3f8eb769ff3eb6619a890a08
mjlbnu <mjlbnu@gmail.com>
2025-05-30 22:36:38 -0300

    fix: return of createPlayers

--- client/src/features/AI/api/AiRequest.js
+++ client/src/features/AI/api/AiRequest.js
@@ -20,7 +20,6 @@ export const getAIResponse = async (prompt) => {
   });
 }
 
-export const createPlayers = (data) => {
-  console.log("Creating players with data:", data);
-  nodeAPI.post("/ai/create-players", data);
+export const createPlayers = async (data) => {
+  return await nodeAPI.post("/ai/create-players", data);
 }
